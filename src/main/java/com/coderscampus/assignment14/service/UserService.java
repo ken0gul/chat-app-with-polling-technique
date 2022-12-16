b5875a05062e99bdf63d3a3e5fc1a3edee686869
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-16 14:48:52 -0500

    Add methods to save and get Users

--- src/main/java/com/coderscampus/assignment14/service/UserService.java
+++ src/main/java/com/coderscampus/assignment14/service/UserService.java
@@ -1,14 +1,27 @@
 package com.coderscampus.assignment14.service;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.coderscampus.assignment14.dto.User;
 import com.coderscampus.assignment14.repository.UserRepository;
 
 @Service
 public class UserService {
 
+	@Autowired
 	private UserRepository userRepository;
 	
 	
+	public void saveUser(User user) {
+		userRepository.save(user);
+		
+	}
+	
+	public Set<User> getUsers() {
+		return userRepository.getUsers();
+	}
 	
 }
