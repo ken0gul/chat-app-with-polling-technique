318a91938f8eb7cee8627831e044f98328a60db8
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-16 14:27:10 -0500

    Add sessionStorage to store everyUser

--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -4,10 +4,24 @@
 let user = {
     'username':`${prompt('What is your name: ')}`
 }
+
+
+if(!sessionStorage.getItem('user')) {
+
 fetch('/welcome', {
     method:'POST',
     headers:{
         "Content-Type":"application/json"
     },
     body:JSON.stringify(user)
-}).then(response => response.json()).then(data => console.log(data)).catch(err=> console.log(err));
+}).then(response => response.json()).then(data => {
+
+
+
+sessionStorage.setItem('user',data)
+
+
+}).catch(err=> console.log(err));
+} else {
+alert('This username is already taken');
+}
