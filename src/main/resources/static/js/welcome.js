2aac570ce8882cef41a8882b0ef06b9f8fdae35b
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-16 14:03:37 -0500

    Create fetch API to get input from prompt and POST it to the given endpoint

--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -1,2 +1,13 @@
 
-let username = prompt('What is your name: ');
+
+
+let user = {
+    'username':`${prompt('What is your name: ')}`
+}
+fetch('/welcome', {
+    method:'POST',
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify(user)
+}).then(response => response.json()).then(data => console.log(data)).catch(err=> console.log(err));
