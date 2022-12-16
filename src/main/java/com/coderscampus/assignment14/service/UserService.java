1dad1a20b47f0c39844f9b457ce2461d4b8aa385
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-16 14:28:55 -0500

    Create UserService

---
+++ src/main/java/com/coderscampus/assignment14/service/UserService.java
@@ -0,0 +1,14 @@
+package com.coderscampus.assignment14.service;
+
+import org.springframework.stereotype.Service;
+
+import com.coderscampus.assignment14.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	private UserRepository userRepository;
+	
+	
+	
+}
