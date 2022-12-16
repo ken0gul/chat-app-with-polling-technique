9045d48937702b82a8d4bd44b695427e1badcd9c
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-16 14:03:03 -0500

    Add PostMapping to get input from the prompt using Fetch API

--- src/main/java/com/coderscampus/assignment14/web/WelcomeController.java
+++ src/main/java/com/coderscampus/assignment14/web/WelcomeController.java
@@ -2,6 +2,11 @@ package com.coderscampus.assignment14.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.coderscampus.assignment14.dto.User;
 
 @Controller
 public class WelcomeController {
@@ -10,5 +15,15 @@ public class WelcomeController {
 	@GetMapping("/welcome")
 	public String getWelcomePage() {
 		return "welcome";
+	}
+	
+	@PostMapping("/welcome")
+	@ResponseBody
+	public User postUsername(@RequestBody User userFromJs) {
+		System.out.println(userFromJs);
+		User user = new User();
+		user.setUsername(userFromJs.getUsername());
+		user.setId(userFromJs.getId());
+		return user;
 	}
 }
