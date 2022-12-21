679625c6a8f20cd4e7bbaa5aac941bba6f43aa3d
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-21 14:54:07 -0500

    Add methods to add and get channels to and from channels list

--- src/main/java/com/coderscampus/assignment14/repository/ChannelRepository.java
+++ src/main/java/com/coderscampus/assignment14/repository/ChannelRepository.java
@@ -2,16 +2,19 @@ package com.coderscampus.assignment14.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Repository;
 
 import com.coderscampus.assignment14.dto.Channel;
+import com.coderscampus.assignment14.dto.User;
 
 @Repository
 public class ChannelRepository {
 	
 	private List<Channel> channels = new ArrayList<>();
-
+	private List<User> users = new ArrayList<>();
+	private Set<User> usersList = null;
 	public Channel findById(Long id) {
 		Channel channel = null;
 		for(int i = 0; i < channels.size(); i++) {
@@ -30,6 +33,18 @@ public class ChannelRepository {
 	
 	public List<Channel> findAll() {
 		return channels;
+	}
+
+	public void saveUser(Channel channel) {
+		channels.add(channel);
+	}
+	
+	public Set<User> getUsers(){
+		
+		channels.forEach(channel -> {
+			usersList = channel.getUsers();
+		});
+		return this.usersList;
 	}
 	
 	
