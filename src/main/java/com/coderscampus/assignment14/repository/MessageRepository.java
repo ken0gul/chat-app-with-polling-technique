9f2525301b6d15f9f878918e1739b30d1efe7482
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-21 14:54:35 -0500

    Add a list for messages

--- src/main/java/com/coderscampus/assignment14/repository/MessageRepository.java
+++ src/main/java/com/coderscampus/assignment14/repository/MessageRepository.java
@@ -1,65 +1,57 @@
 package com.coderscampus.assignment14.repository;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.coderscampus.assignment14.dto.Channel;
+import com.coderscampus.assignment14.dto.Message;
 import com.coderscampus.assignment14.dto.User;
 
 @Repository
 public class MessageRepository {
 
-	private Map<String, List<String>> messages = new HashMap<>();
-	private List<String[]> messageList = new ArrayList<>();
+
+
+
+	private List<Message> list = new ArrayList<>();
 	
-//	public String save(User user) {
-//		UUID id =user.getId();
-//		String username = user.getUsername();
-//		List<String> msgs = user.getMessages();
-//		messages.put(username, msgs);
-//		System.out.println("Message saved in MessageRepo");
-//		return "Message saved";
-//	}
+ 
 	public String save(User user) {
+		Message message = new Message();
+//		 User actualUser = channelRepository.getUsers().stream()
+//				 		.peek(System.out::println)
+//						.filter(u -> u.getChannelId().equals(user.getChannelId()))
+//						.findAny().get();
+		User actualUser = user;
+		
 		UUID id =user.getId();
-		String username = user.getUsername();
-		List<String> msgs = user.getMessages();
+		System.out.println(actualUser);
+		String username = actualUser.getUsername();
+		List<String> msgs = actualUser.getMessages();
 		String[] arr = new String[2];
 		arr[0] = username;
 		arr[1] = msgs.get(msgs.size()-1);
 		String[] anotherArr = new String[2];
 		anotherArr[0]=arr[0];
 		anotherArr[1]=arr[1];
+		message.addMessages(anotherArr);
+		message.setId(user.getChannelId());
+		list.add(message);
 		
-		messageList.add(anotherArr);
-		System.out.println("Message saved in MessageRepo");
 		return "Message saved";
 	}
 	
 
 	
 
-	public  List<String[]> findAll() {
-		return messageList;
+	public  List<Message> findAll() {
+		return list;
 	}
+
 	
-//	public Map<String, List<String>> findAll() {
-//		return messages;
-//	}
-	
-	
-	public List<String> getOnlyMessages() {
-			return messages.entrySet().stream()
-								   .map(item -> item.getValue())
-								   .map(item -> item.toString())
-								   .collect(Collectors.toList());
-								   
-								   
-				
-	}
+
 }
