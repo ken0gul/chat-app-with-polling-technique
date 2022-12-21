9c350af9dc920669ca4b1edcfb86227c48233c30
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-20 19:58:02 -0500

    Create Message.java

---
+++ src/main/java/com/coderscampus/assignment14/dto/Message.java
@@ -0,0 +1,30 @@
+package com.coderscampus.assignment14.dto;
+
+public class Message {
+
+	private Long id;
+	private String message;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", message=" + message + "]";
+	}
+
+	
+}
