f1935f7b995c07c27df5f6a5c0336073e7fd5fb2
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-20 19:58:35 -0500

    Add a list for messages along with setters and getters

--- src/main/java/com/coderscampus/assignment14/dto/User.java
+++ src/main/java/com/coderscampus/assignment14/dto/User.java
@@ -1,11 +1,18 @@
 package com.coderscampus.assignment14.dto;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class User {
 	
 	private UUID id = UUID.randomUUID();
 	private String username;
+	private List<String> messages = new ArrayList<>();
+
+	
+	
+	
 
 	public UUID getId() {
 		return id;
@@ -23,10 +30,21 @@ public class User {
 		this.username = username;
 	}
 
+	
+	public List<String> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(List<String> messages) {
+		this.messages = messages;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + "]";
+		return "User [id=" + id + ", username=" + username + ", messages=" + messages + "]";
 	}
+
+
 	
 	
 }
