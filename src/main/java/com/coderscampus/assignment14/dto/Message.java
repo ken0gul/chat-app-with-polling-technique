d74cc1df516022cc2cec1616f0fee0c18933249c
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-21 14:52:18 -0500

    Update Message.java

--- src/main/java/com/coderscampus/assignment14/dto/Message.java
+++ src/main/java/com/coderscampus/assignment14/dto/Message.java
@@ -1,10 +1,23 @@
 package com.coderscampus.assignment14.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Message {
 
 	private Long id;
 	private String message;
-
+	private  List<String[]> messages = new ArrayList<>();
+	
+	
+	
+	public void addMessages(String[] arr) {
+		messages.add(arr);
+	}
+	
+	public  List<String[]> getMessages() {
+		return messages;
+	}
 	public Long getId() {
 		return id;
 	}
