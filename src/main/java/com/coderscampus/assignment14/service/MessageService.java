c9c7a5cbc86c809480a39ef43bbfb258b3b0b5f7
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-21 14:58:05 -0500

    Remove unused methods

--- src/main/java/com/coderscampus/assignment14/service/MessageService.java
+++ src/main/java/com/coderscampus/assignment14/service/MessageService.java
@@ -1,15 +1,15 @@
 package com.coderscampus.assignment14.service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.coderscampus.assignment14.dto.Channel;
+import com.coderscampus.assignment14.dto.Message;
 import com.coderscampus.assignment14.dto.User;
 import com.coderscampus.assignment14.repository.MessageRepository;
-import com.coderscampus.assignment14.repository.UserRepository;
 
 @Service
 public class MessageService {
@@ -26,12 +26,18 @@ public class MessageService {
 //	public Map<String, List<String>>  getAllMsgs() {
 //		return messageRepository.findAll();
 //	}
-	public List<String[]>  getAllMsgs() {
-		return messageRepository.findAll();
+	public List<String[]>  getAllMsgs(Channel channel) {
+		List<Message> list =  messageRepository.findAll();
+		List<String[]> l = new ArrayList<>();
+		
+		list.forEach(item -> {;
+			if(item.getId().equals(channel.getId())) {
+				l.addAll(item.getMessages());
+			}
+		});
+		return l;
 	}
 	
 	
-	public List<String> getOnlyMessages() {
-		return messageRepository.getOnlyMessages();
-	}
+
 }
