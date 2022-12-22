eaf18dfec31a2e1cce0647dd8a409562dcdd6d56
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-22 15:57:37 -0500

    Update MessageRepository.java

--- src/main/java/com/coderscampus/assignment14/repository/MessageRepository.java
+++ src/main/java/com/coderscampus/assignment14/repository/MessageRepository.java
@@ -14,44 +14,30 @@ import com.coderscampus.assignment14.dto.User;
 @Repository
 public class MessageRepository {
 
-
-
-
 	private List<Message> list = new ArrayList<>();
-	
- 
+
 	public String save(User user) {
 		Message message = new Message();
-//		 User actualUser = channelRepository.getUsers().stream()
-//				 		.peek(System.out::println)
-//						.filter(u -> u.getChannelId().equals(user.getChannelId()))
-//						.findAny().get();
+
 		User actualUser = user;
-		
-		UUID id =user.getId();
-		System.out.println(actualUser);
+
 		String username = actualUser.getUsername();
 		List<String> msgs = actualUser.getMessages();
 		String[] arr = new String[2];
 		arr[0] = username;
-		arr[1] = msgs.get(msgs.size()-1);
+		arr[1] = msgs.get(msgs.size() - 1);
 		String[] anotherArr = new String[2];
-		anotherArr[0]=arr[0];
-		anotherArr[1]=arr[1];
+		anotherArr[0] = arr[0];
+		anotherArr[1] = arr[1];
 		message.addMessages(anotherArr);
 		message.setId(user.getChannelId());
 		list.add(message);
-		
+
 		return "Message saved";
 	}
-	
 
-	
-
-	public  List<Message> findAll() {
+	public List<Message> findAll() {
 		return list;
 	}
-
-	
 
 }
