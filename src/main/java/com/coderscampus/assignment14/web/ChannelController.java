627b9f783841bf03c980fde51b7e4079fdc824d6
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-22 15:59:33 -0500

    Remove unused system.out.printlns

--- src/main/java/com/coderscampus/assignment14/web/ChannelController.java
+++ src/main/java/com/coderscampus/assignment14/web/ChannelController.java
@@ -25,83 +25,63 @@ public class ChannelController {
 
 	@Autowired
 	private ChannelService channelService;
-	
+
 	@Autowired
 	private MessageService messageService;
-	
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping("/channels/{id}")
-	
+
 	public String getChannel(@PathVariable Long id, ModelMap model) {
-		
+
 		Channel foundChannel = channelService.findChannelById(id);
-	
-		
+
 		model.put("channel", foundChannel);
-		// Find the channel
-		System.out.println("Found Channel " + foundChannel);
-		
-		
-		
-		List<User> allUsers = userService.getUsers();
-		model.put("users", allUsers);
-		
-		
-		
-		
-		List<User> user =allUsers;
-		
-		
+
+
+
 		return "channel";
 	}
-	
+
 	@GetMapping("/channels/{id}/messages")
 	@ResponseBody
 	public List<String[]> getChannel2(@PathVariable Long id) {
 		Channel channel = channelService.findChannelById(id);
-	
+
 		return messageService.getAllMsgs(channel);
 	}
-	
-	
-	
+
 	@PostMapping("/channels/{id}")
 	@ResponseBody
 	public User postMessage(@RequestBody User user, @PathVariable Long id) {
-	
-			
+		// Find the current channel
 		Long channelId = user.getChannelId();
 		Channel foundChannel = channelService.findChannelById(id);
-	
 
-		
-		
-		
-		// get user Id
+		// Find the current user
 		UUID userId = user.getId();
-		
 		User actualUser = userService.findById(userId);
+		
+		// Set the channel id of the user
 		actualUser.setChannelId(channelId);
-//		System.out.println("Found User: " + actualUser);
+		
+		// Set messages
 		actualUser.setMessages(user.getMessages());
-		
-		// Save message first
-		
-		
+
+
 		// Add user to the channel
 		foundChannel.addUser(actualUser);
-		
+
 		// add channel to the channels
 		channelService.saveChannelOnPost(foundChannel);
 
-//		
+		
+		// Save user and its message to the repo
 		messageService.saveMessage(actualUser);
-		
+
 		return actualUser;
-		
-		
+
 	}
 }
