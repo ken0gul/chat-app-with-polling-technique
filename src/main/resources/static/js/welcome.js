91ad500e1b5ad17b6faf26f1604a0a2c3829b8e6
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-01-05 17:41:01 -0500

    Convert promise to an async/await function and invoke it only when needed

--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -4,35 +4,59 @@ let userx = JSON.parse(users);
 
 let user;
 
-
+let username;
 
 if(userx == null) {
 
 username = prompt('What is your name?');
-
-} 
-
 user = {
     'username':username
     
-    
 }
+getUser();
+} 
 
 
 
-fetch('/welcome', {
-    method:'POST',
-    headers:{
-        "Content-Type":"application/json"
-    },
-    body:JSON.stringify(user)
-}).then(response => response.json()).then(data => {
 
 
-console.log(data)
-sessionStorage.setItem('user',JSON.stringify(data))
+async function getUser() {
+
+    try {
+
+        let response = await fetch('/welcome', {
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(user)
+        });
+        
+        return await response.json().then(data => sessionStorage.setItem('user',JSON.stringify(data)))
+    }catch(error) {
+        console.error(error);
+    }
+        
+ 
+    
 
 
 
-}).catch(err=> console.log(err));
+
+}
+// fetch('/welcome', {
+//     method:'POST',
+//     headers:{
+//         "Content-Type":"application/json"
+//     },
+//     body:JSON.stringify(user)
+// }).then(response => response.json()).then(data => {
+
+
+// console.log(data)
+// sessionStorage.setItem('user',JSON.stringify(data))
+
+
+
+// }).catch(err=> console.log(err));
 
